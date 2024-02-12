2865ab6c7cf16f0daf0c5be0d8979b4eedc80454
rorivas1981 <rorivas@gmail.com>
2024-02-12 18:05:14 -0300

    FinV2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
@@ -6,9 +5,8 @@ import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailCont
 import { CartProvider } from './context/CartContext';
 import { NotificationProvider } from './notification/NotificationService';
 import CartView from './components/CartView/CartView';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Checkout from './components/Checkout/Checkout';
-import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -24,7 +22,9 @@ function App() {
             <Route path='/checkout' element={<Checkout />} />
             <Route path='*' element={<h1>ERROR 404</h1>} />
           </Routes>
-          <footer className="footer">Todos los derechos reservados por JBL LOVERS - Precios expresados en dolares americanos - No incluyen IVA</footer>
+          <footer style={{ backgroundColor: 'orangered' }} className="footer text-white text-center py-3">
+            Todos los derechos reservados por JBL LOVERS - Precios expresados en dólares americanos - No incluyen IVA
+          </footer>
         </CartProvider>
       </NotificationProvider>
     </BrowserRouter>
